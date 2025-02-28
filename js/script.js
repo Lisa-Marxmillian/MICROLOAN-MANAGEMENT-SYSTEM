bc33ba5b328061ef56b8153fecef502558376878
Lisa-Marxmillian <131626240+Lisa-Marxmillian@users.noreply.github.com>
2025-02-28 12:40:11 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.getElementById("current-year").textContent = new Date().getFullYear();
+        
+        let slideIndex = 0;
+        function showSlides() {
+            let slides = document.getElementsByClassName("slide");
+            for (let i = 0; i < slides.length; i++) 
+            {
+                slides[i].style.display = "none";
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) { slideIndex = 1; }
+            slides[slideIndex - 1].style.display = "block";
+            setTimeout(showSlides, 3000);
+        }
+        showSlides();
